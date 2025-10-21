284c2df9d21de5a8f20015ee7d738fd7fb986916
TheBighi <thebighix@gmail.com>
2025-10-21 09:34:24 +0300

    simplified

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-const express = require('express');
-const posts = require('./data')
-const app = express();
+const express = require("express");
 
+const app = express();
 app.use(express.json());
 
-const { getPosts, getPostByID, postPost,putPost, patchPost, deletePost } = require('./controllers/posts')
-
-app.get('/posts', (req, res) => getPosts(req, res))
-
-app.post('/posts', (req, res) => postPost(req, res))
-
-app.get('/posts/:id', (req, res) => getPostByID(req, res))
-
-app.delete('/posts/:id', (req, res) => deletePost(req, res))
-
-app.patch('/posts/:id/publish', (req, res) => patchPost(req, res))
-
-app.put('/posts/:id', (req, res) => putPost(req, res))
+const postsRoutes = require('./routes/posts.js')
+app.use('/posts', postsRoutes)
 
 app.listen(3001, () => {
-    console.log('Server is running on http://localhost:3001')
-})
+  console.log("Server is running at http://localhos:3001/")
+});
